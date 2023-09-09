727e48a05e1f86a9fa0fa339aaaf54bd9ca720e3
smartvik <vps631532@gmail.com>
2023-09-09 10:35:51 +0530

    Added the check fr inactive item

--- app/src/main/java/com/example/androidtvapp/Constants.kt
+++ app/src/main/java/com/example/androidtvapp/Constants.kt
@@ -1,6 +1,7 @@
 package com.example.androidtvapp
 
 object Constants {
+    val INACTIVE: String = "Inactive"
     const val DASHBOARD_LED = "Dashboard/GetDashbaordData"
     const val BASE_URL = "http://103.240.90.141:5000/service/api/"
     const val NO_INTERNET = "No Internet Connection"
--- app/src/main/java/com/example/androidtvapp/adapter/ListAdapter.kt
+++ app/src/main/java/com/example/androidtvapp/adapter/ListAdapter.kt
@@ -2,7 +2,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.example.androidtvapp.Constants
+import com.example.androidtvapp.Constants.INACTIVE
 import com.example.androidtvapp.R
 import com.example.androidtvapp.model.DashBoardDataResponseItem
 
@@ -14,6 +17,7 @@ class ListAdapter(private val data: List<DashBoardDataResponseItem>?) :
     }
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val actual: TextView = itemView.findViewById(R.id.tv_actual)
+        val rootLayout: View = itemView.findViewById(R.id.root_layout)
         val assembly: TextView = itemView.findViewById(R.id.tv_assembly)
         val line: TextView = itemView.findViewById(R.id.tv_line)
         val target: TextView = itemView.findViewById(R.id.tv_target)
@@ -47,7 +51,12 @@ class ListAdapter(private val data: List<DashBoardDataResponseItem>?) :
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         if (holder is ViewHolder) {
-            val model = data?.get(position)
+            val model = data?.get(position - 1)
+            if (model?.isActive == INACTIVE) {
+                holder.rootLayout.setBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.inactive_item_background))
+            } else {
+                holder.rootLayout.setBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.normal_item_background))
+            }
             holder.assembly.text = model?.displayName
             holder.actual.text = model?.skuCount
             holder.skuCode.text = model?.skuCode
@@ -57,7 +66,7 @@ class ListAdapter(private val data: List<DashBoardDataResponseItem>?) :
     }
 
     override fun getItemCount(): Int {
-        return data?.size!!
+        return data?.size!! + 1
     }
 
     companion object {
